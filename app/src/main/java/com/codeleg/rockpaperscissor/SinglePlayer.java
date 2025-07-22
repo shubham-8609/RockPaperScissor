cb12fb072509034e6a48a496bd539d805cd15d4b
Shubham <shubhamgupta8609@gmail.com>
2025-07-22 14:16:06 +0530

    Little bit animation and code redeability improvement

--- app/src/main/java/com/codeleg/rockpaperscissor/SinglePlayer.java
+++ app/src/main/java/com/codeleg/rockpaperscissor/SinglePlayer.java
@@ -1,238 +1,190 @@
-    package com.codeleg.rockpaperscissor;
+package com.codeleg.rockpaperscissor;
 
-    import android.os.Bundle;
-
-    import android.view.View;
-    import android.view.animation.Animation;
-    import android.view.animation.AnimationUtils;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.AnimationSet;
-    import android.widget.ImageView;
-    import android.widget.LinearLayout;
-    import android.widget.TextView;
-    import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-    import androidx.activity.EdgeToEdge;
-    import androidx.appcompat.app.AppCompatActivity;
-    import androidx.appcompat.widget.AppCompatButton;
-    import androidx.cardview.widget.CardView;
-    import androidx.core.graphics.Insets;
-    import androidx.core.view.ViewCompat;
-    import androidx.core.view.WindowInsetsCompat;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-    public class SinglePlayer extends AppCompatActivity {
-        TextView wonScoreValue;
-        TextView drawScoreValue;
-        TextView loseScoreValue;
-        LinearLayout choosingBox;
-        LinearLayout resultBox;
-        CardView rockButton;
-        CardView paperButton;
-        CardView scissorButton;
-        ImageView userValueImage;
-        ImageView computerValueImage;
-        Animation rotateAnim;
-        LinearLayout mainElement;
-        TextView resultText;
-        Animation scaleAnim;
-        AppCompatButton playAgainButton;
-        Animation slideanim;
-        Animation fadeInAnim;
-        int wonCount = 0;
-        int drawnCount = 0;
-        int loseCount = 0;
-        Game game = new Game();
+public class SinglePlayer extends AppCompatActivity {
+    TextView wonScoreValue;
+    TextView drawScoreValue;
+    TextView loseScoreValue;
+    LinearLayout choosingBox;
+    LinearLayout resultBox;
+    CardView rockButton;
+    CardView paperButton;
+    CardView scissorButton;
+    ImageView userValueImage;
+    ImageView computerValueImage;
+    Animation rotateAnim;
+    LinearLayout mainElement;
+    TextView resultText;
+    Animation scaleAnim;
+    AppCompatButton playAgainButton;
+    Animation slideanim;
+    Animation fadeInAnim;
+    int wonCount = 0;
+    int drawnCount = 0;
+    int loseCount = 0;
+    Game game = new Game();
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            EdgeToEdge.enable(this);
-            setContentView(R.layout.activity_single_player);
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                return insets;
-            });
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_single_player);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-            init();
+        init();
 
-            rockButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    handlePlayerChoice("rock");
-                }
-            });
-
-            paperButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    handlePlayerChoice("paper");
-                }
-            });
-
-            scissorButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    handlePlayerChoice("scissor");
-                }
-            });
-        }
-
-        public void init(){
-            this.wonScoreValue = findViewById(R.id.won_score_value);
-            this.drawScoreValue = findViewById(R.id.draw_score_value);
-            this.loseScoreValue = findViewById(R.id.lose_score_value);
-            this.rockButton = findViewById(R.id.rock_button);
-            this.paperButton = findViewById(R.id.paper_button);
-            this.choosingBox = findViewById(R.id.choosing_box);
-            this.mainElement = findViewById(R.id.main);
-            this.resultBox = findViewById(R.id.result_box);
-            this.scissorButton = findViewById(R.id.scissor_button);
-            this.userValueImage = findViewById(R.id.user_value);
-            this.computerValueImage = findViewById(R.id.computer_value);
-            this.playAgainButton = findViewById(R.id.play_again_button);
-            this.resultText  = findViewById(R.id.result_text);
-            this.slideanim = AnimationUtils.loadAnimation(getApplicationContext() , R.anim.slide_anim);
-            this.rotateAnim = AnimationUtils.loadAnimation(getApplicationContext() , R.anim.rotate_anim);
-            this.scaleAnim = AnimationUtils.loadAnimation(getApplicationContext() , R.anim.scale_anim);
-            this.fadeInAnim = AnimationUtils.loadAnimation(getApplicationContext() , R.anim.fade_in);
-
-
-
-        }
-
-        public void handlePlayerChoice(String userChoosenOption) {
-            String computerChoosenOption = ""; // Initialize with a default value
-            int computerChoice = game.generateRandom();
-
-            switch (computerChoice){
-                case 0:
-                    computerChoosenOption = "rock";
-                    break;
-                case 1:
-                    computerChoosenOption = "paper";
-                    break;
-                case 2 :
-                    computerChoosenOption = "scissor";
-            }
-
-            int wonState = checkWinner(userChoosenOption , computerChoosenOption);
-            switch (wonState){
-                case 0:
-                    break;
-                case 1:
-                    break;
-                case 2:
-            }
-            changeLayout();
-            showAnimations(wonState, userChoosenOption , computerChoosenOption);
-        }
-
-        public int checkWinner(String userChoosenOption , String computerChoosenOption){
-            int state; // 0 --> draw , 1 --> userWon , 2 ---> computer Won..
-            if (userChoosenOption.equals(computerChoosenOption)){
-                state = 0;
-                resultText.setText("Game Drawn..");
-                drawnCount++;
-                setValues(state);
-
-                resultText.setTextColor(getResources().getColor(R.color.orange));
-
-            }else if((userChoosenOption.equals("rock") && computerChoosenOption.equals("paper"))  || (userChoosenOption.equals("paper") && (computerChoosenOption.equals("scissor"))) || (userChoosenOption.equals("scissor") && computerChoosenOption.equals("rock"))){
-                state = 2;
-                loseCount++;
-                setValues(state);
-                resultText.setText("You lose..😟🥺");
-                resultText.setTextColor(getResources().getColor(R.color.red));
-               // Example: Set background to red for a loss
-            }else{
-                state = 1;
-                wonCount++;
-                setValues(state);
-                resultText.setText("You Won..🥳🎉");
-                resultText.setTextColor(getResources().getColor(R.color.pure_green));
-             // Example: Set background to green for a win
-            }
-            return state;
-        }
-        public void changeLayout(){
-            choosingBox.setVisibility(View.GONE);
-            resultBox.setVisibility(View.VISIBLE);
-        }
-        public void showAnimations(int wonState , String userChoosenOption , String computerChoosenOption){
-            int userImageResource = 0;
-            int computerImageResource = 0;
-
-            switch (userChoosenOption) {
-                case "rock":
-                    userImageResource = R.drawable.rock_big; // Assuming you have rock.png, rock.xml etc. in drawable
-
-                    break;
-                case "paper":
-                    userImageResource = R.drawable.paper_big; // Assuming you have paper.png, paper.xml etc. in drawable
-
-                    break;
-                case "scissor":
-                    userImageResource = R.drawable.scissor_big; // Assuming you have scissor.png, scissor.xml etc. in drawable
-
-                    break;
-            }
-
-            switch (computerChoosenOption) {
-                case "rock":
-                    computerImageResource = R.drawable.rock_big;
-                    break;
-                case "paper":
-                    computerImageResource = R.drawable.paper_big;
-                    break;
-                case "scissor":
-                    computerImageResource = R.drawable.scissor_big;
-                    break;
-            }
-
-            userValueImage.setImageResource(userImageResource);
-            computerValueImage.setImageResource(computerImageResource);
-
-            AnimationSet computerAnimationSet = new AnimationSet(true);
-            computerAnimationSet.addAnimation(rotateAnim);
-            computerAnimationSet.addAnimation(scaleAnim);
-
-            AnimationSet userAnimationSet = new AnimationSet(true);
-            userAnimationSet.addAnimation(rotateAnim);
-            userAnimationSet.addAnimation(scaleAnim);
-
-            computerValueImage.startAnimation(computerAnimationSet);
-            userValueImage.startAnimation(userAnimationSet);
-            resultText.startAnimation(slideanim);
-        }
-
-       public  void playAgain(View view){
-            resultBox.setVisibility(View.GONE);
-            choosingBox.setVisibility(View.VISIBLE);
-
-            choosingBox.startAnimation(fadeInAnim);
-            choosingBox.startAnimation(scaleAnim);
-
-        }
-        void setValues(int state){
-            switch (state){
-                case 0:
-            drawScoreValue.setText(String.valueOf(drawnCount));
-            drawScoreValue.startAnimation(slideanim);
-            drawScoreValue.setTextColor(getResources().getColor(R.color.orange));
-                    break;
-                case 1:
-            wonScoreValue.setText(String.valueOf(wonCount));
-            wonScoreValue.startAnimation(slideanim);
-                    wonScoreValue.setTextColor(getResources().getColor(R.color.pure_green));
-
-            break;
-                case 2:
-            loseScoreValue.setText(String.valueOf(loseCount));
-            loseScoreValue.startAnimation(slideanim);
-            loseScoreValue.setTextColor(getResources().getColor(R.color.red));
-            break;
-
-            }
-        }
-
+        rockButton.setOnClickListener(v -> handlePlayerChoice(Choice.ROCK));
+        paperButton.setOnClickListener(v -> handlePlayerChoice(Choice.PAPER));
+        scissorButton.setOnClickListener(v -> handlePlayerChoice(Choice.SCISSOR));
     }
+
+    public void init() {
+        this.wonScoreValue = findViewById(R.id.won_score_value);
+        this.drawScoreValue = findViewById(R.id.draw_score_value);
+        this.loseScoreValue = findViewById(R.id.lose_score_value);
+        this.rockButton = findViewById(R.id.rock_button);
+        this.paperButton = findViewById(R.id.paper_button);
+        this.choosingBox = findViewById(R.id.choosing_box);
+        this.mainElement = findViewById(R.id.main);
+        this.resultBox = findViewById(R.id.result_box);
+        this.scissorButton = findViewById(R.id.scissor_button);
+        this.userValueImage = findViewById(R.id.user_value);
+        this.computerValueImage = findViewById(R.id.computer_value);
+        this.playAgainButton = findViewById(R.id.play_again_button);
+        this.resultText = findViewById(R.id.result_text);
+        this.slideanim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_anim);
+        this.rotateAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_anim);
+        this.scaleAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.scale_anim);
+        this.fadeInAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+    }
+
+    public void handlePlayerChoice(Choice userChosenOption) {
+        int computerChoiceIndex = game.generateRandom();
+        Choice computerChosenOption = Choice.values()[computerChoiceIndex];
+
+        int wonState = checkWinner(userChosenOption, computerChosenOption);
+
+        changeLayout();
+        setupChoiceImages(userChosenOption, computerChosenOption);
+        startChoiceAnimations();
+    }
+
+    // Returns: 0 = Draw, 1 = Win, 2 = Lose
+    public int checkWinner(Choice userChosenOption, Choice computerChosenOption) {
+        int state;
+        if (userChosenOption == computerChosenOption) {
+            state = 0;
+            resultText.setText("Game Drawn..");
+            drawnCount++;
+            setValues(state);
+            drawScoreValue.setTextColor(ContextCompat.getColor(this, R.color.orange));
+        } else if (
+                (userChosenOption == Choice.ROCK && computerChosenOption == Choice.PAPER) ||
+                        (userChosenOption == Choice.PAPER && computerChosenOption == Choice.SCISSOR) ||
+                        (userChosenOption == Choice.SCISSOR && computerChosenOption == Choice.ROCK)
+        ) {
+            state = 2;
+            loseCount++;
+            setValues(state);
+            resultText.setText("You lose..😟🥺");
+            loseScoreValue.setTextColor(ContextCompat.getColor(this, R.color.red));
+        } else {
+            state = 1;
+            wonCount++;
+            setValues(state);
+            resultText.setText("You Won..🥳🎉");
+            wonScoreValue.setTextColor(ContextCompat.getColor(this, R.color.pure_green));
+        }
+        return state;
+    }
+
+    public void changeLayout() {
+        choosingBox.setVisibility(View.GONE);
+        resultBox.setVisibility(View.VISIBLE);
+    }
+
+    // Helper to set images for both players
+    private void setupChoiceImages(Choice userChoice, Choice computerChoice) {
+        userValueImage.setImageResource(getChoiceDrawable(userChoice));
+        computerValueImage.setImageResource(getChoiceDrawable(computerChoice));
+    }
+
+    // Helper to start animations for both images and result text
+    private void startChoiceAnimations() {
+        AnimationSet animSet = new AnimationSet(true);
+        animSet.addAnimation(rotateAnim);
+        animSet.addAnimation(scaleAnim);
+
+        computerValueImage.startAnimation(animSet);
+        userValueImage.startAnimation(animSet);
+        resultText.startAnimation(slideanim);
+    }
+
+    private int getChoiceDrawable(Choice choice) {
+        switch (choice) {
+            case ROCK:
+                return R.drawable.rock_big;
+            case PAPER:
+                return R.drawable.paper_big;
+            case SCISSOR:
+                return R.drawable.scissor_big;
+            default:
+                return 0;
+        }
+    }
+
+    public void playAgain(View view) {
+        resultBox.setVisibility(View.GONE);
+        choosingBox.setVisibility(View.VISIBLE);
+
+        choosingBox.startAnimation(fadeInAnim);
+        choosingBox.startAnimation(scaleAnim);
+    }
+
+    void setValues(int state) {
+        switch (state) {
+            case 0:
+                drawScoreValue.setText(String.valueOf(drawnCount));
+                drawScoreValue.startAnimation(slideanim);
+                drawScoreValue.setTextColor(ContextCompat.getColor(this, R.color.orange));
+                break;
+            case 1:
+                wonScoreValue.setText(String.valueOf(wonCount));
+                wonScoreValue.startAnimation(slideanim);
+                wonScoreValue.setTextColor(ContextCompat.getColor(this, R.color.pure_green));
+                break;
+            case 2:
+                loseScoreValue.setText(String.valueOf(loseCount));
+                loseScoreValue.startAnimation(slideanim);
+                loseScoreValue.setTextColor(ContextCompat.getColor(this, R.color.red));
+                break;
+        }
+    }
+
+    public enum Choice {
+        ROCK,
+        PAPER,
+        SCISSOR
+    }
+}
--- app/src/main/java/com/codeleg/rockpaperscissor/SplashActivity.java
+++ app/src/main/java/com/codeleg/rockpaperscissor/SplashActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,13 +17,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.Random;
-
 public class SplashActivity extends AppCompatActivity {
-Intent iNext;
-TextView creditText;
+    Intent iNext;
+    TextView creditText;
+    LottieAnimationView lottieAnimView;
+    Animation textAnim;
+    View rootLayout;
 
-Animation textAnim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,28 +33,64 @@ Animation textAnim;
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-
         });
+
         init();
 
-                creditText.startAnimation(textAnim);
+        // Fade in whole splash screen
+        rootLayout.setAlpha(0f);
+        rootLayout.animate().alpha(1f).setDuration(400).start();
 
+        // Animate credit text (pop-in)
+        creditText.setScaleX(0.7f);
+        creditText.setScaleY(0.7f);
+        creditText.setAlpha(0f);
+        creditText.animate()
+                .alpha(1f)
+                .scaleX(1f)
+                .scaleY(1f)
+                .setDuration(650)
+                .setStartDelay(180)
+                .start();
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                startActivity(iNext);
-                finish();
-            }
-        } , 1000);
-
-
-    }
-        public  void init(){
-        iNext = new Intent(SplashActivity.this , MainActivity.class);
-        creditText = findViewById(R.id.credit_text);
-        textAnim = AnimationUtils.loadAnimation(this , R.anim.splash_text_anim);
+        // Animate Lottie view for entrance (optional, feels premium)
+        if (lottieAnimView != null) {
+            lottieAnimView.setScaleX(0.88f);
+            lottieAnimView.setScaleY(0.88f);
+            lottieAnimView.animate()
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .setDuration(400)
+                    .setStartDelay(250)
+                    .start();
         }
 
+        // Splash duration
+        int splashDuration = 1600; // 1.6 seconds
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            // Fade out before starting next activity
+            rootLayout.animate()
+                    .alpha(0f)
+                    .setDuration(250)
+                    .withEndAction(() -> {
+                        startActivity(iNext);
+                        finish();
+                    })
+                    .start();
+        }, splashDuration);
+    }
+
+    public void init() {
+        iNext = new Intent(SplashActivity.this, MainActivity.class);
+        creditText = findViewById(R.id.credit_text);
+        lottieAnimView = findViewById(R.id.lottie_animation_view); // update xml id accordingly
+        textAnim = AnimationUtils.loadAnimation(this, R.anim.splash_text_anim);
+        rootLayout = findViewById(R.id.main);
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//        // Prevent back during splash
+//    }
 }
